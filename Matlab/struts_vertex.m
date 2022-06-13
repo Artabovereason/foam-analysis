8f816d12a2e6912ddcccf427caa5c3b9f81e3b0b
Pierre Guichard <64526173+Artabovereason@users.noreply.github.com>
2022-06-13 15:02:38 +0200

    Update struts_vertex.m

--- Matlab/struts_vertex.m
+++ Matlab/struts_vertex.m
@@ -78,78 +78,129 @@ an.FitBoxToText='on';
 drawnow
 exportgraphics(fig_slh,strcat(true_name,'/strut-length-distribution.png'));
 
+
+
+
+
 %%%%%%%%%%%%%%%%%%
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%% fluctuations de l'angle dièdre (écart aux 109 degres..)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-les109degres=acos(-1/3)/pi*180;
-binning=30;
-xmv=linspace(0.9*xmin_v,1.1*xmax_v,binning);
-ymv=linspace(0.9*ymin_v,1.1*ymax_v,binning);
-zmv=linspace(0.9*zmin_v,1.1*zmax_v,binning);
 
-
-density109=zeros(binning,binning+1,binning);
-density109_alongz=zeros(binning,2);
-densityv_alongz=zeros(binning,1);
-density_anomalousv_alongz=zeros(binning,1);
+les109degres              = acos(-1/3)/pi*180;
+binning                   = 30;
+xmv                       = linspace(0.9*xmin_v,1.1*xmax_v,binning);
+ymv                       = linspace(0.9*ymin_v,1.1*ymax_v,binning);
+zmv                       = linspace(0.9*zmin_v,1.1*zmax_v,binning);
+density109                = zeros(binning,binning+1,binning);
+density109_alongz         = zeros(binning,2);
+density109_anomalousv_alongz = zeros(binning,2);
+densityv_alongz           = zeros(binning,1);
+density_anomalousv_alongz = zeros(binning,1);
 
 for j=1:numel(xg_v)
    % if(mod(j,floor(numel(xg_v)/10))==0) disp(num2str(j/n_struts));end
-    ux=find(xmv>=xg_v(j),1);
-    uy=find(ymv>=yg_v(j),1); 
-    uz=find(zmv>=zg_v(j),1);
+    ux = find(xmv>=xg_v(j),1);
+    uy = find(ymv>=yg_v(j),1); 
+    uz = find(zmv>=zg_v(j),1);
     
-    vertexabsoluteindex=vertices_alive(j);
-	tmplines=find(strutsmat_unfolded==vertexabsoluteindex);
-	vertex_order=nnz(tmplines);
+    vertexabsoluteindex = vertices_alive(j);
+	tmplines            = find(strutsmat_unfolded==vertexabsoluteindex);
+    vertex_order        = nnz(tmplines);
 	if(vertex_order~=4)
-		density_anomalousv_alongz(uz)=density_anomalousv_alongz(uz)+1;
-	else
-		angle=0;angle2=0;
-		tmplines(tmplines>n_struts)=tmplines(tmplines>n_struts)-n_struts;
+        angle                       = 0;
+        angle2                      = 0;
+		tmplines(tmplines>n_struts) = tmplines(tmplines>n_struts)-n_struts;
 		for k=1:3
-			j1=struts{tmplines(k)}(1);j2=struts{tmplines(k)}(2);
+			j1 = struts{tmplines(k)}(1);
+            j2 = struts{tmplines(k)}(2);
 			if (j2==vertexabsoluteindex) j2=j1;end
 			%on est obligé d'utiliser vertices_COM et non pas xg_v...
-			s1x=vertices_COM{j2}(1)-vertices_COM{vertexabsoluteindex}(1);
-			s1y=vertices_COM{j2}(2)-vertices_COM{vertexabsoluteindex}(2);
-			s1z=vertices_COM{j2}(3)-vertices_COM{vertexabsoluteindex}(3);
+			s1x = vertices_COM{j2}(1)-vertices_COM{vertexabsoluteindex}(1);
+			s1y = vertices_COM{j2}(2)-vertices_COM{vertexabsoluteindex}(2);
+			s1z = vertices_COM{j2}(3)-vertices_COM{vertexabsoluteindex}(3);
+			for kk=k+1:vertex_order
+				j3=struts{tmplines(kk)}(1);
+                j4=struts{tmplines(kk)}(2);
+				if (j4==vertexabsoluteindex) j4=j3;end
+				s2x   = vertices_COM{j4}(1)-vertices_COM{vertexabsoluteindex}(1);
+				s2y   = vertices_COM{j4}(2)-vertices_COM{vertexabsoluteindex}(2);
+				s2z   = vertices_COM{j4}(3)-vertices_COM{vertexabsoluteindex}(3);
+				lecos = (s1x*s2x+s1y*s2y+s1z*s2z)/sqrt(s1x^2+s1y^2+s1z^2)/sqrt(s2x^2+s2y^2+s2z^2);
+				if(abs(lecos)>1) lecos=sign(lecos)*min(abs(lecos),1);end
+				angle  = angle+acos(lecos)/pi*180;
+				angle2 = angle2+(acos(lecos)/pi*180)^2;
+			end
+		end
+		density109(ux,uy,uz)    = density109(ux,uy,uz)+sqrt(angle2/6-(angle/6)^2)/(angle/6);
+		density109_anomalousv_alongz(uz,1) = density109_anomalousv_alongz(uz,1)+angle/6;
+		density109_anomalousv_alongz(uz,2) = density109_anomalousv_alongz(uz,2)+sqrt(angle2/6-(angle/6)^2)/(angle/6);	
+        density_anomalousv_alongz(uz) = density_anomalousv_alongz(uz)+1;
+	else
+		angle                       = 0;
+        angle2                      = 0;
+		tmplines(tmplines>n_struts) = tmplines(tmplines>n_struts)-n_struts;
+		for k=1:3
+			j1 = struts{tmplines(k)}(1);
+            j2 = struts{tmplines(k)}(2);
+			if (j2==vertexabsoluteindex) j2=j1;end
+			%on est obligé d'utiliser vertices_COM et non pas xg_v...
+			s1x = vertices_COM{j2}(1)-vertices_COM{vertexabsoluteindex}(1);
+			s1y = vertices_COM{j2}(2)-vertices_COM{vertexabsoluteindex}(2);
+			s1z = vertices_COM{j2}(3)-vertices_COM{vertexabsoluteindex}(3);
 			for kk=k+1:4
 				j3=struts{tmplines(kk)}(1);j4=struts{tmplines(kk)}(2);
 				if (j4==vertexabsoluteindex) j4=j3;end
-				s2x=vertices_COM{j4}(1)-vertices_COM{vertexabsoluteindex}(1);
-				s2y=vertices_COM{j4}(2)-vertices_COM{vertexabsoluteindex}(2);
-				s2z=vertices_COM{j4}(3)-vertices_COM{vertexabsoluteindex}(3);
-				lecos=(s1x*s2x+s1y*s2y+s1z*s2z)/sqrt(s1x^2+s1y^2+s1z^2)/sqrt(s2x^2+s2y^2+s2z^2);
+				s2x   = vertices_COM{j4}(1)-vertices_COM{vertexabsoluteindex}(1);
+				s2y   = vertices_COM{j4}(2)-vertices_COM{vertexabsoluteindex}(2);
+				s2z   = vertices_COM{j4}(3)-vertices_COM{vertexabsoluteindex}(3);
+				lecos = (s1x*s2x+s1y*s2y+s1z*s2z)/sqrt(s1x^2+s1y^2+s1z^2)/sqrt(s2x^2+s2y^2+s2z^2);
 				if(abs(lecos)>1) lecos=sign(lecos)*min(abs(lecos),1);end
-				angle=angle+acos(lecos)/pi*180;
-				angle2=angle2+(acos(lecos)/pi*180)^2;
+				angle  = angle+acos(lecos)/pi*180;
+				angle2 = angle2+(acos(lecos)/pi*180)^2;
 			end
 		end
-		density109(ux,uy,uz)=density109(ux,uy,uz)+sqrt(angle2/6-(angle/6)^2)/(angle/6);
-		density109_alongz(uz,1)=density109_alongz(uz,1)+angle/6;
-		density109_alongz(uz,2)=density109_alongz(uz,2)+sqrt(angle2/6-(angle/6)^2)/(angle/6);		
-		densityv_alongz(uz)=densityv_alongz(uz)+1;
+		density109(ux,uy,uz)    = density109(ux,uy,uz)+sqrt(angle2/6-(angle/6)^2)/(angle/6);
+		density109_alongz(uz,1) = density109_alongz(uz,1)+angle/6;
+		density109_alongz(uz,2) = density109_alongz(uz,2)+sqrt(angle2/6-(angle/6)^2)/(angle/6);		
+		densityv_alongz(uz)     = densityv_alongz(uz)+1;
 	end
 end
 %%% visu
 fig_order_vrtx=figure('visible','off');
 set(fig_order_vrtx,'position',[ 23        1616        1000         438]);
 subplot(2,1,1)
-plotyy(zmv,densityv_alongz,zmv,density_anomalousv_alongz);
-title('raw counting of order 4 and order <>4 vertices');
-xlabel('z');
+%plotyy(zmv,densityv_alongz,zmv,density_anomalousv_alongz);
+hold on
+plot(zmv,densityv_alongz,'x-','LineWidth',2);  
+plot(zmv,density_anomalousv_alongz,'x-','LineWidth',2); 
+legend({'Vertex of order $=4$','Vertex of order $\neq 4$'},'Interpreter','Latex');
+
+hold off 
+title('Counting of order $=4$ and order $\neq 4$ vertices','Interpreter','Latex');
+xlabel('$z$ position','Interpreter','Latex');
 subplot(2,1,2)
-plotyy(zmv,density109_alongz(:,1)./max(1,densityv_alongz)/les109degres,zmv,density109_alongz(:,2)./max(1,densityv_alongz))
-title('local mean dihedral angle and standard deviation')
-xlabel('z');
+%plotyy(zmv,density109_alongz(:,1)./max(1,densityv_alongz)/les109degres,zmv,density109_alongz(:,2)./max(1,densityv_alongz))
+hold on
+%errorbar(zmv,density109_alongz(:,1)./max(1,densityv_alongz)/les109degres                     ,density109_alongz(:,2)./max(1,densityv_alongz),'LineWidth',2)
+%errorbar(zmv,density109_anomalousv_alongz(:,1)./max(1,density_anomalousv_alongz)/les109degres,density109_anomalousv_alongz(:,2)./max(1,density_anomalousv_alongz),'LineWidth',2)
+plot(zmv,(density109_alongz(:,1)./max(1,densityv_alongz))/les109degres  ,'x-','LineWidth',2)
+plot(zmv,density109_anomalousv_alongz(:,1)./max(1,density_anomalousv_alongz)/les109degres,'x-','LineWidth',2)
+plot([min(zmv),max(zmv)], [90/les109degres, 90/les109degres])
+plot([min(zmv),max(zmv)], [mean((density109_alongz(:,1)./max(1,densityv_alongz))/les109degres ), mean((density109_alongz(:,1)./max(1,densityv_alongz))/les109degres )])
+plot([min(zmv),max(zmv)], [mean(density109_anomalousv_alongz(:,1)./max(1,density_anomalousv_alongz)/les109degres), mean(density109_anomalousv_alongz(:,1)./max(1,density_anomalousv_alongz)/les109degres)])
+hold off
+legend({'Vertex of order $=4$','Vertex of order $\neq 4$','$90/109.5$','$\langle$ order $=4\rangle$','$\langle$ order $\neq 4\rangle$'},'Interpreter','Latex');
+ylim([0,2])
+title('Local mean dihedral angle distribution','Interpreter','Latex')
+xlabel('$z$ position','Interpreter','Latex');
 exportgraphics(fig_order_vrtx,strcat(true_name,'/order_vertex.png'));
 
-%% The 3D plot of the struts
-
+disp('The mean angle for vertex of order different from 4 is :');
+disp(mean(density109_anomalousv_alongz(:,1)./max(1,density_anomalousv_alongz)/les109degres));
+%%
 test_list = zeros(numel(vertices_COM),3);
 test_x = zeros(numel(vertices_COM),1);
 test_y = zeros(numel(vertices_COM),1);
@@ -162,19 +213,6 @@ for i=1:numel(vertices_COM)
     test_y(i) = vertices_COM{i}(2);
     test_z(i) = vertices_COM{i}(3);
 end
-
-%{
-fff=figure;
-hold on
-
-for i=1:numel(struts)
-    plot3([test_x(struts{i}(1)),test_x(struts{i}(2))],[test_y(struts{i}(1)),test_y(struts{i}(2))],[test_z(struts{i}(1)),test_z(struts{i}(2))])
-
-end
-
-hold off
-%plot3(test_x,test_y,test_z,'x');
-%}
 
 
 %% Length of the struts as a function of z
@@ -269,127 +307,73 @@ disp(mean(connectivity_order_vertex_fixed));
 %%
 histogram_order_vertex = figure('Visible','off');
 histogram(connectivity_order_vertex);
+xlabel('vertex order','Interpreter','latex');
 exportgraphics(histogram_order_vertex,strcat(true_name,'/histogram_order_vertex.png'));
 
 histogram_order_vertex2 = figure('Visible','off');
 histogram(connectivity_order_vertex_fixed);
+xlabel('vertex order','Interpreter','latex');
 exportgraphics(histogram_order_vertex2,strcat(true_name,'/histogram_order_vertex_fixed.png'));
 
-%%
+%% topological g(n)
+
 %{
-max_n_order     = 6;
-topological_g_r = zeros(max_n_order,1);
-setdif_contact = [];
-% for n=1, the first value of g(r) is the order of the vertex
-%topological_g_r(end+1)=1;%connectivity_order_vertex(index_start);
-up_to = numel(list_pertinent_index_vertices);
+max_n_order         = 7;
+All_n_connectionn_n = cell(max_n_order,1);
+All_n_connectionn   = cell(max_n_order,1);
+topological_g_r     = zeros(max_n_order-1,1);
+
+up_to = 400;%numel(list_pertinent_index_vertices);
 for k = 1:up_to
-    index_start = list_pertinent_index_vertices(k);
-    index =index_start;
-    first_vertex_start = vertices_COM{index_start,1};
-    already_tagged_vertex = {};
+    index_start                  = list_pertinent_index_vertices(k);
+    index                        = index_start;
+    first_vertex_start           = vertices_COM{index_start,1};
+    already_tagged_vertex        = {};
     already_tagged_vertex{end+1} = index_start;
-    n_connection0_0 = [];
-    n_connection0_0 = [n_connection0_0,index];
-    n_connection1 = find([struts{:}] == index);
-    already_done_connection = [];
-    already_done_connection = [already_done_connection,index];
-    
-    %%%
-    n_connection1_1 = [];
-    for i = 1:numel(n_connection1)
-        already_done_connection(end+1) = struts{fix(n_connection1(i)/2) +rem(n_connection1(i),2)}( rem(n_connection1(i),2)+1 );
-        n_connection1_1(end+1)         = struts{fix(n_connection1(i)/2) +rem(n_connection1(i),2)}( rem(n_connection1(i),2)+1 );
+    All_n_connectionn{1}         = [index];
+    All_n_connectionn{2}         = find([struts{:}] == index);    
+    already_done_connection      = [];
+    already_done_connection      = [already_done_connection,index];
+    setdif_contact               = [];
+    All_n_connectionn_n{2}       = [];
+    for i = 1:numel(All_n_connectionn{2})
+        already_done_connection(end+1) = struts{fix(All_n_connectionn{2}(i)/2) +rem(All_n_connectionn{2}(i),2)}( rem(All_n_connectionn{2}(i),2)+1 );
+        All_n_connectionn_n{2}(end+1)  = struts{fix(All_n_connectionn{2}(i)/2) +rem(All_n_connectionn{2}(i),2)}( rem(All_n_connectionn{2}(i),2)+1 );
     end
-    n_connection1_1 = unique(n_connection1_1);
-    topological_g_r(1)=topological_g_r(1)+numel(n_connection1_1); 
-    setdif_contact = [setdif_contact,n_connection1_1];
-    
-    %%%
-    n_connection2_2 = [];
-    for i = 1:numel(n_connection1_1)
-        n_connection2 = find([struts{:}] == n_connection1_1(i));
-        for i = 1:numel(n_connection2)
-            already_done_connection(end+1) = struts{fix(n_connection2(i)/2) +rem(n_connection2(i),2)}( rem(n_connection2(i),2)+1 );
-            n_connection2_2(end+1)         = struts{fix(n_connection2(i)/2) +rem(n_connection2(i),2)}( rem(n_connection2(i),2)+1 );
+    All_n_connectionn_n{2}       = unique(All_n_connectionn_n{2});
+    topological_g_r(1)           = topological_g_r(1)+numel(All_n_connectionn_n{2}); 
+    setdif_contact               = [setdif_contact,All_n_connectionn_n{2}];
+    for w = 2:max_n_order-1
+        All_n_connectionn_n{w+1} = [];
+        for i = 1:numel(All_n_connectionn_n{w})
+            All_n_connectionn{w+1} = find([struts{:}] == All_n_connectionn_n{w}(i));
+            for i = 1:numel(All_n_connectionn{w+1})
+                already_done_connection(end+1)  = struts{fix(All_n_connectionn{w+1}(i)/2) +rem(All_n_connectionn{w+1}(i),2)}( rem(All_n_connectionn{w+1}(i),2)+1 );
+                All_n_connectionn_n{w+1}(end+1) = struts{fix(All_n_connectionn{w+1}(i)/2) +rem(All_n_connectionn{w+1}(i),2)}( rem(All_n_connectionn{w+1}(i),2)+1 );
+            end
         end
-    end
-    n_connection2_2         = unique(n_connection2_2);
-    n_connection2_2         = setdiff(n_connection2_2,[n_connection0_0,n_connection1_1]);
-    already_done_connection = unique(already_done_connection);
-    topological_g_r(2)  = topological_g_r(2)+numel(n_connection2_2);
-    
-    %%%
-    n_connection3_3 = [];
-    for i = 1:numel(n_connection2_2)
-        n_connection3 = find([struts{:}] == n_connection2_2(i));
-        for i = 1:numel(n_connection3)
-            already_done_connection(end+1) = struts{fix(n_connection3(i)/2) +rem(n_connection3(i),2)}( rem(n_connection3(i),2)+1 );
-            n_connection3_3(end+1)         = struts{fix(n_connection3(i)/2) +rem(n_connection3(i),2)}( rem(n_connection3(i),2)+1 );
-        end
-    end
-    n_connection3_3         = unique(n_connection3_3);
-    n_connection3_3         = setdiff(n_connection3_3,[n_connection0_0,n_connection1_1,n_connection2_2]);
-    already_done_connection = unique(already_done_connection);
-    topological_g_r(3)  =topological_g_r(3) + numel(n_connection3_3);
-
-    %%%
-    n_connection4_4 = [];
-    for i = 1:numel(n_connection3_3)
-        n_connection4 = find([struts{:}] == n_connection3_3(i));
-        for i = 1:numel(n_connection4)
-            already_done_connection(end+1) = struts{fix(n_connection4(i)/2) +rem(n_connection4(i),2)}( rem(n_connection4(i),2)+1 );
-            n_connection4_4(end+1)         = struts{fix(n_connection4(i)/2) +rem(n_connection4(i),2)}( rem(n_connection4(i),2)+1 );
-        end
-    end
-    n_connection4_4         = unique(n_connection4_4);
-    n_connection4_4         = setdiff(n_connection4_4,[n_connection0_0,n_connection1_1,n_connection2_2,n_connection3_3]);
-    already_done_connection = unique(already_done_connection);
-    topological_g_r(4)  =topological_g_r(4)+ numel(n_connection4_4);
-
-    %%%
-    n_connection5_5 = [];
-    for i = 1:numel(n_connection4_4)
-        n_connection5 = find([struts{:}] == n_connection4_4(i));
-        for i = 1:numel(n_connection5)
-            already_done_connection(end+1) = struts{fix(n_connection5(i)/2) +rem(n_connection5(i),2)}( rem(n_connection5(i),2)+1 );
-            n_connection5_5(end+1)         = struts{fix(n_connection5(i)/2) +rem(n_connection5(i),2)}( rem(n_connection5(i),2)+1 );
-        end
-    end
-    n_connection5_5         = unique(n_connection5_5);
-    n_connection5_5         = setdiff(n_connection5_5,[n_connection0_0,n_connection1_1,n_connection2_2,n_connection3_3,n_connection4_4]);
-    already_done_connection = unique(already_done_connection);
-    topological_g_r(5)  = topological_g_r(5)+numel(n_connection5_5);
-
-    %%%
-    n_connection6_6 = [];
-    for i = 1:numel(n_connection5_5)
-        n_connection6 = find([struts{:}] == n_connection5_5(i));
-        for i = 1:numel(n_connection6)
-            already_done_connection(end+1) = struts{fix(n_connection6(i)/2) +rem(n_connection6(i),2)}( rem(n_connection6(i),2)+1 );
-            n_connection6_6(end+1)         = struts{fix(n_connection6(i)/2) +rem(n_connection6(i),2)}( rem(n_connection6(i),2)+1 );
-        end
-    end
-    n_connection6_6         = unique(n_connection6_6);
-    n_connection6_6         = setdiff(n_connection6_6,[n_connection0_0,n_connection1_1,n_connection2_2,n_connection3_3,n_connection4_4,n_connection5_5]);
-    already_done_connection = unique(already_done_connection);
-    topological_g_r(6)  = topological_g_r(6)+numel(n_connection6_6); 
-
-    %%%
+        All_n_connectionn_n{w+1} = unique(All_n_connectionn_n{w+1});
+        All_n_connectionn_n{w+1} = setdiff(All_n_connectionn_n{w+1},[All_n_connectionn{1},setdif_contact]);
+        already_done_connection  = unique(already_done_connection);
+        topological_g_r(w)       = topological_g_r(w)+numel(All_n_connectionn_n{w+1});
+        setdif_contact           = [setdif_contact, All_n_connectionn_n{w+1}];
+    end   
 end
-
 for i = 1:numel(topological_g_r)
     topological_g_r(i) = (1/up_to)* topological_g_r(i)/(i^2);
 end
 
 topological_g_r_fig=figure('visible','off');
+hold on
 plot(1:numel(topological_g_r),topological_g_r,'x-','LineWidth',2);
+hold off
 xlabel('number of step $n$','Interpreter','Latex');
 ylabel('number of unique vertex','Interpreter','Latex');
-ylim([0,mean(connectivity_order_vertex_fixed)]);
+ylim([0,4]);
 title('Topological $g(n)$','Interpreter','Latex');
 exportgraphics(topological_g_r_fig,strcat(true_name,'/topological_g_r.png'));
 %}
+
 
 
 %{
@@ -411,5 +395,46 @@ for idx = 1:10
     end
 end
 tt
+%}
+
+
+%% The 3D plot of the struts
+
+
+%{
+
+[fi, rep] = uigetfile('*.tif');
+chemin = fullfile(rep, '*.tif');
+list = dir(chemin); 
+
+mkdir(strcat(true_name,'/slices'));
+for j = 1:1013
+    fff=figure(Visible="off");
+    image = fullfile(rep, list(j).name);
+    I = imread(image);
+    imshow(I);    
+    hold on
+    cache = 0 ;
+    for i=1:numel(list_pertinent_index_vertices)
+        if vertices_COM{list_pertinent_index_vertices(i),1}(3) == j
+            %plot3([test_x(struts{i}(1)),test_x(struts{i}(2))],[test_y(struts{i}(1)),test_y(struts{i}(2))],[test_z(struts{i}(1)),test_z(struts{i}(2))])
+            %plot([test_x(struts{i}(1)),test_x(struts{i}(2))],[test_y(struts{i}(1)),test_y(struts{i}(2))])
+            scatter(vertices_COM{list_pertinent_index_vertices(i),1}(1),vertices_COM{list_pertinent_index_vertices(i),1}(2),'x','LineWidth',2);
+            cache = cache+1;
+        end
+    end
+    xlim([0, 1812])
+    ylim([0, 1198])
+    xlabel('$x$ position [pixel]','Interpreter','latex')
+    ylabel('$y$ position [pixel]','Interpreter','latex')
+    
+    hold off
+    if cache ~= 0
+        name = strcat('/','slice',num2str(j),'.png');
+        exportgraphics(fff,strcat(true_name,'/slices',name));
+    end
+
+end
+%plot3(test_x,test_y,test_z,'x');
 %}
 
